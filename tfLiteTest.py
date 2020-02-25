d49bd3e37e630967b6d85c89443ef06a4ff688c9
Lei Wang <c6177841383@gmail.com>
2020-02-25 18:05:49 -0500

    change hard coded input image to .png by type

--- tfLiteTest.py
+++ tfLiteTest.py
@@ -1,5 +1,5 @@
 from data import *
 
 # To convert to tflite model from Keras: tflite_convert --output_file=unet_xxx.tflite --keras_model_file=unet_xxx.hdf5
-test_single_image = test_image_prep('0.png')
-test_lite_img = load_model_lite_single_predict('unet_gscpLaser228x448-rd32.tflite', test_single_image)
+test_single_image = test_image_prep('Solar0.png')
+test_lite_img = load_model_lite_single_predict('unet_Solar512x512-rd32.tflite', test_single_image)
